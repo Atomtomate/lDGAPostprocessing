37d5aecd8a23ea4beb6470c429bf3484387fb73a
hhpstobb <hhpstobb@blogin6.usr.hlrn.de>
2021-07-25 16:02:04 +0200

    expand vertex memory usage reduction

--- expand_vertex.jl
+++ expand_vertex.jl
@@ -13,17 +13,48 @@ dataPath = ARGS[2] # "/scratch/usr/hhpstobb/lDGA/tests/non_qubic/ed_vertex"
 β = parse(Float64, ARGS[3])
 
 println("opening: ", gridPath*"/freqList.jld2")
-f = jldopen(gridPath*"/freqList.jld2", "r")
-for k in keys(f)
-    s=Symbol(k)
-    @eval (($s) = ($(f[k])))
+jldopen(gridPath*"/freqList.jld2", "r") do f
+    for k in ["freqList", "nFermi", "nBose", "shift"] #keys(f)
+        s=Symbol(k)
+        @eval (($s) = ($(f[k])))
+    end
 end
 
 #@load gridPath*"/freqList.jld2" freqRed_map freqList freqList_min parents ops nFermi nBose shift base offset
-
 println("Expanding Vertex for nFermi=",nFermi,", nBose=",nBose,", shift=",shift)
-base2 = base*base
-freq_red, TwoPartGF_upup_red, TwoPartGF_updo_red = read_vert_chi(dataPath*"/2_part_gf_red") 
+flush(stderr)
+flush(stdout)
+
+function expand_TwoPartGF(freqFile, dataPath)
+
+    jldopen(gridPath*"/freqList.jld2", "r") do f
+        for k in ["freqRed_map", "parents", "ops"]
+            s=Symbol(k)
+            @eval (($s) = ($(f[k])))
+        end
+    end
+
+    transform_f(F_up::Array{T,1}, F_do::Array{T,1}, prev::Int, next::Int, ops::Array{Int,1}) where T = 
+                   if ops[next] == 1 
+                       F_up[next] = conj(F_up[prev])
+                       F_do[next] = conj(F_do[prev])
+                   elseif ops[next] == 3 || ops[next] == 4
+                       F_up[next] = -F_up[prev]
+                       F_do[next] = F_do[prev] - F_up[prev]
+                   else
+                       F_up[next] = F_up[prev]
+                       F_do[next] = F_do[prev]
+                   end
+    freq_red, TwoPartGF_upup_red, TwoPartGF_updo_red = read_vert_chi(dataPath*"/2_part_gf_red") 
+    TwoPartGF_upup, TwoPartGF_updo = expand(TwoPartGF_upup_red, TwoPartGF_updo_red, transform_f, freqRed_map, freqList, parents, ops, nBose, nFermi);
+    return TwoPartGF_upup, TwoPartGF_updo
+end
+
+TwoPartGF_upup, TwoPartGF_updo =  expand_TwoPartGF(gridPath*"/freqList.jld2", dataPath)
+println("Done expanding!")
+flush(stderr)
+flush(stdout)
+
 
 gImp = read_gm_wim(2*(nBose+nFermi+1), dataPath*"/gm_wim", storedInverse=false)
 g0 = 1.0 ./ read_gm_wim(2*(nBose+nFermi+1), dataPath*"/g0mand", storedInverse=false)
@@ -31,19 +62,6 @@ g0 = 1.0 ./ read_gm_wim(2*(nBose+nFermi+1), dataPath*"/g0mand", storedInverse=fa
 #F_den, F_mag = SparseVertex.computeF(freq_full, sv_up_test_full, sv_do_test_full, χ0_full)
 #ind = indices(sv_up_test_full) 
 
-transform_f(F_up::Array{T,1}, F_do::Array{T,1}, prev::Int, next::Int, ops::Array{Int,1}) where T = 
-                               if ops[next] == 1 
-                                   F_up[next] = conj(F_up[prev])
-                                   F_do[next] = conj(F_do[prev])
-                               elseif ops[next] == 3 || ops[next] == 4
-                                   F_up[next] = -F_up[prev]
-                                   F_do[next] = F_do[prev] - F_up[prev]
-                               else
-                                   F_up[next] = F_up[prev]
-                                   F_do[next] = F_do[prev]
-                               end
-
-TwoPartGF_upup, TwoPartGF_updo = expand(TwoPartGF_upup_red, TwoPartGF_updo_red, transform_f, freqRed_map, freqList, parents, ops, nBose, nFermi);
 
 SparseVertex.subtract_ω0!(freqList, TwoPartGF_upup, gImp, β)
 SparseVertex.subtract_ω0!(freqList, TwoPartGF_updo, gImp, β)
