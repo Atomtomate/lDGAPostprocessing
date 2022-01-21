a155e1de391ea30c1b1cbacee399af950440b276
Julian Stobbe <jstobbe@physnet.uni-hamburg.de>
2022-01-21 11:41:24 +0100

    expand_vertex bug fixes

--- expand_vertex.jl
+++ expand_vertex.jl
@@ -55,49 +55,47 @@ println("Done expanding!")
 flush(stderr)
 flush(stdout)
 
-gImp    = read_gm_wim(2*(nBose+nFermi+1), dataPath*"/gm_wim", storedInverse=false)
-g0      = 1.0 ./ read_gm_wim(2*(nBose+nFermi+1), dataPath*"/g0mand", storedInverse=false)
+gImp    = read_gm_wim(4*(nBose+nFermi+1), dataPath*"/gm_wim", storedInverse=false)
+g0      = 1.0 ./ read_gm_wim(4*(nBose+nFermi+1), dataPath*"/g0mand", storedInverse=false)
 χ0_full = lDGAPostprocessing.computeχ0(-nBose:nBose, -(nFermi+shift*nBose):(nFermi+shift*nBose)-1, gImp, β)
 #F_den, F_mag = lDGAPostprocessing.computeF(freq_full, sv_up_test_full, sv_do_test_full, χ0_full)
 #ind = indices(sv_up_test_full) 
 
+    Γch     = -1.0 .* lDGAPostprocessing.computeΓ(freqList, TwoPartGF_upup .+ TwoPartGF_updo, χ0_full,nBose,nFermi)
+    Γsp     = -1.0 .* lDGAPostprocessing.computeΓ(freqList, TwoPartGF_upup .- TwoPartGF_updo, χ0_full,nBose,nFermi)
+    lDGAPostprocessing.add_χ₀!(freqList, TwoPartGF_upup, gImp, β)
+    lDGAPostprocessing.add_χ₀!(freqList, TwoPartGF_updo, gImp, β)
+    println("Done calculating vertex!")
+    flush(stderr)
+    flush(stdout)
+    # TODO: activate this via write_fortran flag
+    #lDGAPostprocessing.write_vert_chi(freqList, TwoPartGF_upup, TwoPartGF_updo, dataPath, 2*nBose+1, 2*nFermi)
+    #lDGAPostprocessing.write_fort_dir("gamma", freqList, -Γch, -Γsp, dataPath*"/gamma_dir", 2*nBose+1, 2*nFermi)
+    #lDGAPostprocessing.write_fort_dir("chi", freqList, TwoPartGF_upup, TwoPartGF_updo, dataPath*"/chi_dir", 2*nBose+1, 2*nFermi)
+    # TODO: find a way to keep memory consumption low
 
-χDMFTch = TwoPartGF_upup .+ TwoPartGF_updo
-χDMFTsp = TwoPartGF_upup .- TwoPartGF_updo
-Γch     = -1.0 .* lDGAPostprocessing.computeΓ(freqList, χDMFTch, χ0_full,nBose,nFermi)
-Γsp     = -1.0 .* lDGAPostprocessing.computeΓ(freqList, χDMFTsp, χ0_full,nBose,nFermi)
-lDGAPostprocessing.add_χ₀!(freqList, TwoPartGF_upup, gImp, β)
-lDGAPostprocessing.add_χ₀!(freqList, TwoPartGF_updo, gImp, β)
+    println("tmp: after permute")
+    flush(stderr)
+    flush(stdout)
 
-println("Done calculating vertex!")
-flush(stderr)
-flush(stdout)
-# TODO: activate this via write_fortran flag
-lDGAPostprocessing.write_vert_chi(freqList, TwoPartGF_upup, TwoPartGF_updo, dataPath, 2*nBose+1, 2*nFermi)
-lDGAPostprocessing.write_fort_dir("gamma", freqList, -Γch, -Γsp, dataPath*"/gamma_dir", 2*nBose+1, 2*nFermi)
-lDGAPostprocessing.write_fort_dir("chi", freqList, TwoPartGF_upup, TwoPartGF_updo, dataPath*"/chi_dir", 2*nBose+1, 2*nFermi)
-# TODO: find a way to keep memory consumption low
-TwoPartGF_updo = nothing
-TwoPartGF_upup = nothing
 
-χDMFTch = permutedims(reshape(χDMFTch, 2*nFermi, 2*nFermi, 2*nBose+1),[3,2,1])
-χDMFTsp = permutedims(reshape(χDMFTsp, 2*nFermi, 2*nFermi, 2*nBose+1),[3,2,1])
+    ϵₖ, Vₖ, μ    = read_anderson_parameters(dataPath * "/hubb.andpar");
+    U, β, nden   = read_hubb_dat(dataPath * "/hubb.dat");
+    iνₙ, GImp_ED = readGImp(dataPath*"/gm_wim", only_positive=true)
+    E_kin_DMFT, E_pot_DMFT  = calc_E_ED(iνₙ[1:length(GImp_ED)], ϵₖ, Vₖ, GImp_ED, nden, U, β, μ)
+    res = isfile(dataPath * "/chi_asympt") ? read_chi_asympt(dataPath * "/chi_asympt") : ([], [], [])
+    χ_ch_asympt, χ_sp_asympt, χ_pp_asympt = res
 
-ϵₖ, Vₖ, μ    = read_anderson_parameters(dataPath * "/hubb.andpar");
-U, β, nden   = read_hubb_dat(dataPath * "/hubb.dat");
-iνₙ, GImp_ED = readGImp(dataPath*"/gm_wim", only_positive=true)
-E_kin_DMFT, E_pot_DMFT  = calc_E_ED(iνₙ[1:length(GImp_ED)], ϵₖ, Vₖ, GImp_ED, nden, U, β, μ)
-res = isfile(dataPath * "/chi_asympt") ? read_chi_asympt(dataPath * "/chi_asympt") : ([], [], [])
-χ_ch_asympt, χ_sp_asympt, χ_pp_asympt = res
 
-gImp = read_gm_wim(10*(nBose+nFermi+1), dataPath*"/gm_wim", storedInverse=false)
-g0 = 1.0 ./ read_gm_wim(10*(nBose+nFermi+1), dataPath*"/g0mand", storedInverse=false)
+    println("tmp: before write")
+    flush(stderr)
+    flush(stdout)
 
 jldopen(dataPath*"/ED_out.jld2", "w") do f
     f["Γch"] = Γch
     f["Γsp"] = Γsp
-    f["χDMFTch"] = χDMFTch
-    f["χDMFTsp"] = χDMFTsp
+    f["χDMFTch"] = permutedims(reshape(TwoPartGF_upup .+ TwoPartGF_updo, 2*nFermi, 2*nFermi, 2*nBose+1),[3,2,1])
+    f["χDMFTsp"] = permutedims(reshape(TwoPartGF_upup .- TwoPartGF_updo, 2*nFermi, 2*nFermi, 2*nBose+1),[3,2,1])
     f["χ_ch_asympt"] = χ_ch_asympt
     f["χ_sp_asympt"] = χ_sp_asympt
     f["χ_pp_asympt"] = χ_pp_asympt
@@ -111,7 +109,6 @@ jldopen(dataPath*"/ED_out.jld2", "w") do f
     f["nden"] = nden
     f["E_kin_DMFT"] = E_kin_DMFT
     f["E_pot_DMFT"] = E_pot_DMFT
-    f["FUpDo"] = FUpDo_from_χDMFT(0.5 .* (χDMFTch .- χDMFTsp), gImp, gridPath*"/freqList.jld2", β)
     f["grid_shift"] = shift
     f["grid_nBose"] = nBose
     f["grid_nFermi"] = nFermi
